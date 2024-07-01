a27a2c9a6e5a97a052a8f6c54f6dcd6cb25f27e6
rhuamanh <rhuamanh@unjbg.edu.pe>
2024-07-01 10:59:24 -0500

    ejercicio 2

---
+++ arreglos2.cpp
@@ -0,0 +1,28 @@
+//ejercicio de arreglos 2
+#include <iostream>
+using namespace std;
+int main (){
+	int n, i, j, num, p, r, z;
+	int arreglo [n];
+	cout << "ingrese la cantidad de elementos:";
+	cin >> n;
+	for (i=0; i<=n; i++){
+		cout << "ingrese el numero del elemento:";
+		cin >> num;
+		p=0;
+		r=0;
+		if (num % 2 ==0){
+			for (j=0; j<=p; j++){
+				int arreglo [j];
+			}
+		} else{
+			for (z=0; z=r; z++){
+				int arreglo [z];
+			}
+		}
+	}
+	cout << arreglo [n] << endl << endl;
+	cout << arreglo [j] << endl << endl;
+	cout << arreglo [z] << endl << endl;
+	return 0;
+}
